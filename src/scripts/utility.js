1d9a2f1e6e899e0d1c2425ee965d7d02b41ef328
Cameron Brown <jupiter49777@gmail.com>
2026-01-08 22:47:38 +0200

    feat: Add createList and createContainer functions
    createList handles returning a list and createContainer handles creating a div with a heading element

--- src/scripts/utility.js
+++ src/scripts/utility.js
@@ -4,3 +4,19 @@ export function createElement(type, className, content) {
   element.textContent = content;
   return element;
 }
+
+export function createList(listItems) {
+  const list = createElement('ul', 'content-container__list');
+  listItems.forEach((item) => {
+    const listItem = createElement('li', 'hours-list__item', item);
+    list.appendChild(listItem);
+  });
+  return list;
+}
+
+export function createContainer(containerTitle) {
+  const container = createElement('div', 'content-container');
+  const title = createElement('h2', 'content-container__title', containerTitle);
+  container.append(title);
+  return container;
+}
